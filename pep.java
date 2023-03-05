15d3ae7827d7bc45e46d33bd56b61ebb9d38459e
Sahidhusen Mukdiwala <shahidhusenmukdiwala1@gmail.com>
2023-03-05 20:48:32 +0530

    second

---
+++ pep.java
@@ -0,0 +1,13 @@
+class pep {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in)
+        System.out.println("Enter your frist name: ");
+        String fristname = scan.next()
+
+        System.out.println("Enter your last name: ");
+        String lastname = scan.next()
+
+        System.out.println("your full name is: " + fristname +'' +lastname);
+    }
+}
